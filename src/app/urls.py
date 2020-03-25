ca0d17ed724e6b71942a199973a774c25df4cd8f
codingcat <istommao@gmail.com>
2020-03-25 23:16:43 +0800

    Add app filepond api

--- src/app/urls.py
+++ src/app/urls.py
@@ -3,5 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('filepond/', views.filepond_view),
+    path('view/filepond/', views.filepond_view),
+    path('api/filepond/process', views.filepond_process_api),
+    path('api/filepond/revert', views.filepond_revert_api),
+    path('api/filepond/restore', views.filepond_restore_api),
+    path('api/filepond/load', views.filepond_load_api),
+    path('api/filepond/fetch', views.filepond_fetch_api)
 ]
--- src/app/views.py
+++ src/app/views.py
@@ -1,5 +1,37 @@
+"""app views."""
+
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+
+from django.views.decorators.csrf import csrf_exempt
 
 
 def filepond_view(request):
     return render(request, 'filepond.html')
+
+
+@csrf_exempt
+def filepond_process_api(request):
+    """upload files."""
+    return JsonResponse({'ok': 'ok'})
+
+
+@csrf_exempt
+def filepond_revert_api(request):
+    """revert upload."""
+    return JsonResponse({'ok': 'ok'})
+
+
+@csrf_exempt
+def filepond_restore_api(request):
+    return JsonResponse({'ok': 'ok'})
+
+
+@csrf_exempt
+def filepond_load_api(request):
+    return JsonResponse({'ok': 'ok'})
+
+
+@csrf_exempt
+def filepond_fetch_api(request):
+    return JsonResponse({'ok': 'ok'})
